fa30bd295abfa3e92aae04e33ae8484a06b7bf27
Richard Hoogstad <rshoogstad@gmail.com>
2015-08-29 17:20:35 +0200

    added tulsie site fixtures

---
+++ src/Tulsie/Bundle/PageBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tulsie\Bundle\PageBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadUserData extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
+{
+    private $container;
+
+    function getOrder()
+    {
+        return 1;
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $manager = $this->getUserManager();
+
+        $user = $manager->createUser();
+        $user->setUsername('admin');
+        $user->setEmail('admin@test.com');
+        $user->setPlainPassword('admin');
+        $user->setEnabled(true);
+        $user->setSuperAdmin(true);
+        $user->setLocked(false);
+
+        $manager->updateUser($user);
+
+    }
+
+    /**
+     * @return \FOS\UserBundle\Model\UserManagerInterface
+     */
+    public function getUserManager()
+    {
+        return $this->container->get('fos_user.user_manager');
+    }
+}
