dea8609e72307f4c3a1707d75e6384f16ab8cb9a
Richard Hoogstad <rshoogstad@gmail.com>
2015-09-28 20:52:10 +0200

    adjustment for bash file

--- tulsie_start.bash
+++ tulsie_start.bash
@@ -5,7 +5,9 @@ app/console doctrine:database:create
 app/console doctrine:schema:create
 app/console doctrine:fixtures:load --fixtures=src/Tulsie/Bundle/PageBundle/DataFixtures/ORM
 app/console sonata:page:create-site
-app/console assets/install
+app/console sonata:page:update-core-routes --site=all
+app/console sonata:page:create-snapshots --site=all
+
 
 cd web
 bower install ../vendor/sonata-project/admin-bundle/bower.json
